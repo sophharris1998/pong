c44329ea84bea816ed62a11d69e4ded8f23731bf
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2022-11-28 22:25:39 +0000

    added ID

---
+++ script.js
@@ -0,0 +1,13 @@
+// =create index, sass, JS,
+//score 1, score 2
+// ball
+// player paddle 1, player paddle 2
+// paddle positions
+//update loop for everytime something changes
+// x and y
+// different directions
+// bounce from walls and paddles
+//ball speeding up
+//score changing
+
+const ball = ball(document.getElementById("ball"));

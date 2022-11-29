8cf3b8e2649a12ec9846fb2378c0d52246432064
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2022-11-29 15:33:26 +0000

    paddle event listener

--- script.js
+++ script.js
@@ -3,11 +3,17 @@
 // ball
 // player paddle 1, player paddle 2
 // paddle positions
-//update loop for everytime something changes
+//update loop for everytime something changes so it constantly runs, move ball, check for unputs, EVERYTHING
+// onInput/onChange everytime you type in a box itll relate back to the id/class. up button makes paddle go up and down button makes it go down
 // x and y
 // different directions
 // bounce from walls and paddles
 //ball speeding up
 //score changing
 
-const ball = ball(document.getElementById("ball"));
+const game = document.querySelector(".game");
+const movingPaddlePrimary = () => {
+  console.log("It works!");
+};
+
+game.addEventListener("mousemove", movingPaddlePrimary);
